0aef0dee8c5d721e9cf6123ef376e1a9319e62ca
252624 <252624@astanait.edu.kz>
2026-01-23 17:39:20 +0500

    sql Inital commit

--- repositories/UserRepository.java
+++ repositories/UserRepository.java
@@ -1,93 +1,45 @@
-package com.company.repositories;
+package repositories;
 
-import com.company.data.interfaces.IDB;
-import com.company.models.User;
-import com.company.repositories.interfaces.IUserRepository;
+import data.interfaces.IDB;
+import models.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserRepository implements IUserRepository {
-    private final IDB db;  // Dependency Injection
+public class UserRepository {
+
+    private final IDB db;
 
     public UserRepository(IDB db) {
         this.db = db;
     }
 
-    @Override
-    public boolean createUser(User user) {
-        Connection con = null;
+    // Get all users from database
+    public List<User> findAll() {
+        List<User> users = new ArrayList<>();
+        String sql = "SELECT id, name, rating FROM users";
 
-        try {
-            con = db.getConnection();
-            String sql = "INSERT INTO users(name,surname,gender) VALUES (?,?,?)";
-            PreparedStatement st = con.prepareStatement(sql);
+        try (Connection con = db.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
-            st.setString(1, user.getName());
-            st.setString(2, user.getSurname());
-            st.setBoolean(3, user.getGender());
-
-            st.execute();
-
-            return true;
-        } catch (SQLException e) {
-            System.out.println("sql error: " + e.getMessage());
-        }
-
-        return false;
-    }
-
-    @Override
-    public User getUser(int id) {
-        Connection con = null;
-
-        try {
-            con = db.getConnection();
-            String sql = "SELECT id,name,surname,gender FROM users WHERE id=?";
-            PreparedStatement st = con.prepareStatement(sql);
-
-            st.setInt(1, id);
-
-            ResultSet rs = st.executeQuery();
-            if (rs.next()) {
-                return new User(rs.getInt("id"),
-                        rs.getString("name"),
-                        rs.getString("surname"),
-                        rs.getBoolean("gender"));
-            }
-        } catch (SQLException e) {
-            System.out.println("sql error: " + e.getMessage());
-        }
-
-        return null;
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-        Connection con = null;
-
-        try {
-            con = db.getConnection();
-            String sql = "SELECT id,name,surname,gender FROM users";
-            Statement st = con.createStatement();
-
-            ResultSet rs = st.executeQuery(sql);
-            List<User> users = new ArrayList<>();
             while (rs.next()) {
-                User user = new User(rs.getInt("id"),
+                User user = new User(
+                        rs.getInt("id"),
                         rs.getString("name"),
-                        rs.getString("surname"),
-                        rs.getBoolean("gender"));
-
+                        rs.getInt("rating")
+                );
                 users.add(user);
             }
 
-            return users;
         } catch (SQLException e) {
-            System.out.println("sql error: " + e.getMessage());
+            e.printStackTrace();
         }
 
-        return null;
+        return users;
     }
 }
