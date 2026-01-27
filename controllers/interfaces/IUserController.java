e4d3576a394b21531e43b11473bb7aeada217994
User <253157@astanait.edu.kz>
2026-01-27 09:28:42 +0500

    deleting and creating new

--- controllers/interfaces/IUserController.java
+++
@@ -1,7 +0,0 @@
-package com.company.controllers.interfaces;
-
-public interface IFeedbackController {
-    String createFeedback(String name, int rating, String comment);
-    String getFeedback(int id);
-    String getAllFeedbacks();
-}
